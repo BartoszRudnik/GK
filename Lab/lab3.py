fadbcad9b2e089ac94e52c06f95304d5ad8c7c52
Bartosz Rudnik <rudnik49@gmail.com>
2020-11-20 12:50:35 +0100

    refactor, comments

--- Lab/lab3.py
+++ Lab/lab3.py
@@ -6,37 +6,45 @@ import numpy as np
 from OpenGL.GL import *
 from glfw.GLFW import *
 
-N = 50
+N = 100
 
 triangleColors = np.random.rand(N, N, 3)
 
+
+# funkcja sluzaca do wyliczania wspolrzednych jajka
 def computeEgg(N):
     tab = np.zeros((N, N, 3))
 
     tmp = 1 / (N - 1)
 
     for i in range(N):
+        #wyznaczenie nowej wartosci U z przedzialu [0,1]
         U = i * tmp
         for j in range(N):
+            #wyznaczenie nowej wartosci V z przedzialu [0,1]
             V = j * tmp
 
+            #wyznaczenie wspolrzednej X,Y i Z na podstawie wzoru
             X = (-90 * pow(U, 5) + 225 * pow(U, 4) - 270 * pow(U, 3) + 180 * pow(U, 2) - 45 * U) * math.cos(math.pi * V)
             Y = 160 * pow(U, 4) - 320 * pow(U, 3) + 160 * pow(U, 2)
             Z = (-90 * pow(U, 5) + 225 * pow(U, 4) - 270 * pow(U, 3) + 180 * pow(U, 2) - 45 * U) * math.sin(math.pi * V)
 
             tab[i, j, 0] = X
+            #przesuniecie wspolrzednej Y tak by model znalazl sie na srodku ukladu wspolrzednych
             tab[i, j, 1] = Y - 5
             tab[i, j, 2] = Z
 
     return tab
 
 
+#funkcja umozliwiajaca zanimowanie obiektu i jego lepsza obserwacje
 def spin(angle):
     glRotatef(angle, 1.0, 0.0, 0.0)
     glRotatef(angle, 0.0, 1.0, 0.0)
     glRotatef(angle, 0.0, 0.0, 1.0)
 
 
+#funkcja tworzaca model jajka przy pomocy punktów
 def pointsEgg():
     tab = computeEgg(N)
     glBegin(GL_POINTS)
@@ -46,11 +54,13 @@ def pointsEgg():
     glEnd()
 
 
+#funkcja tworzaca model jajka przy pomocy linii
 def linesEgg():
     tab = computeEgg(N)
     glBegin(GL_LINES)
     for i in range(N - 1):
         for j in range(N - 1):
+            #element (i,j) łaczymy z elementami (i + 1, j) i (i, j + 1)
             glVertex3f(tab[i, j, 0], tab[i, j, 1], tab[i, j, 2])
             glVertex3f(tab[i + 1, j, 0], tab[i + 1, j, 1], tab[i + 1, j, 2])
 
@@ -60,6 +70,7 @@ def linesEgg():
     glEnd()
 
 
+#funkcja tworzaca model jajka przy pomocy trojkatow
 def trianglesEgg():
     tab = computeEgg(N)
 
@@ -67,6 +78,7 @@ def trianglesEgg():
 
     for i in range(N - 1):
         for j in range(N - 1):
+            #element (i,j) łaczony jest z elementami (i + 1, j) oraz (i, j + 1)
             glColor3f(triangleColors[i, j, 0], triangleColors[i, j, 1], triangleColors[i, j, 2])
             glVertex3f(tab[i, j, 0], tab[i, j, 1], tab[i, j, 2])
 
@@ -76,6 +88,7 @@ def trianglesEgg():
             glColor3f(triangleColors[i, j + 1, 0], triangleColors[i, j + 1, 1], triangleColors[i, j + 1, 2])
             glVertex3f(tab[i, j + 1, 0], tab[i, j + 1, 1], tab[i, j + 1, 2])
 
+            #wyznaczenie trojkata dopelniajacego
             glColor3f(triangleColors[i + 1, j + 1, 0], triangleColors[i + 1, j + 1, 1], triangleColors[i + 1, j + 1, 2])
             glVertex3f(tab[i + 1, j + 1, 0], tab[i + 1, j + 1, 1], tab[i + 1, j + 1, 2])
 
@@ -88,6 +101,7 @@ def trianglesEgg():
     glEnd()
 
 
+#funkcja tworzaca model jajka przy pomocy prymitywu paskowego
 def trianglesStripEgg():
     tab = computeEgg(N)
 
@@ -108,20 +122,25 @@ def trianglesStripEgg():
     glEnd()
 
 
+#funkcja sluzaca do wyliczenia wspolrzednych torusa
 def computeTorus(N, R, r, moveX, moveY, moveZ):
     tab = np.zeros((N, N, 3))
 
     tmp = 1 / (N - 1)
 
     for i in range(N):
+        #wyznaczenie nowej wartosci U z przedzialu [0,1]
         U = i * tmp
         for j in range(N):
+            #wyznaczenie nowej wartosci V z przedzialu [0,1]
             V = j * tmp
 
+            #wyznaczenie wspolrzednych X, Y, Z
             X = (R + r * math.cos(2 * math.pi * V)) * math.cos(2 * math.pi * U)
             Y = (R + r * math.cos(2 * math.pi * V)) * math.sin(2 * math.pi * U)
             Z = r * math.sin(2 * math.pi * V)
 
+            #zmienne moveX, moveY, moveZ sluza do przesuwania polozenia punktow
             tab[i, j, 0] = X + moveX
             tab[i, j, 1] = Y + moveY
             tab[i, j, 2] = Z + moveZ
@@ -129,6 +148,7 @@ def computeTorus(N, R, r, moveX, moveY, moveZ):
     return tab
 
 
+#stworzenie modelu torusa za pomoca punktow
 def pointsTorus(moveX, moveY, moveZ):
     tab = computeTorus(N, 1.3, 0.2, moveX, moveY, moveZ)
     glBegin(GL_POINTS)
@@ -138,17 +158,20 @@ def pointsTorus(moveX, moveY, moveZ):
     glEnd()
 
 
+#funckja tworzaca model lancucha zlozonego z torusów
 def drawChain():
-    glColor3f(1.0, 0.0, 0.0)
-
+    #wspolrzedne srodka pierwszego z torusow
     vertical = -8.0
-    horizontal = -6
+    horizontal = -1.0
 
     for i in range(7):
+
+        glColor3f(triangleColors[i, i, 0], triangleColors[i, i, 1], triangleColors[i, i, 2])
 
         vertical += 2
         horizontal += 0.2
 
+        # w zaleznosci od tego czy torus bedzie obrocony czy nie stosujemy inne przesuniecie
         if (i % 2 == 0):
             pointsTorus(horizontal, vertical, 0)
         else:
@@ -190,12 +213,22 @@ def render(time):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
+    # wywolanie funkcji spin
     spin(time * 180 / math.pi)
-    # pointsEgg()
-    # linesEgg()
-    # trianglesEgg()
-    trianglesStripEgg()
 
+    # wywolanie funkcji tworzacej model jajka przy pomocy punktów
+    pointsEgg()
+
+    # wywolanie funkcji tworzacej model jajka przy pomocy linii
+    # linesEgg()
+
+    # wywolanie funkcji tworzacej model jajka przy pomocy trojkatow
+    # trianglesEgg()
+
+    # wywolanie funkcji tworzacej model jajka przy pomocy prymitywu paskowego
+    # trianglesStripEgg()
+
+    # wywołanie funkcji tworzacej lancuch przy pomocy torusow
     # drawChain()
 
     axes()
